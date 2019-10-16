f15de6201ecf8b0b64bc8672da05d83d1ac8ef23
Maick Souza <maick_a_s@msn.com>
2019-10-15 22:34:43 -0300

    reactotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+import history from './services/history';
 
 function App() {
   return (
-    <div className="App">
-      <h1>NearMaickHackingHello</h1>
-    </div>
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
 
